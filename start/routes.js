35da1fe41b6efa77fe8067b1d5452c0ac16285df
wesleyvicen <wesley1535@hotmail.com>
2020-03-10 10:18:51 -0300

    adicionado controller e funcionando o post de cadastro de usuário

--- start/routes.js
+++ start/routes.js
@@ -5,3 +5,5 @@ const Route = use("Route");
 Route.resource("anuncio", "AnuncioController").apiOnly();
 
 Route.resource("noticia", "NoticiaController").apiOnly();
+
+Route.post("/users", "UserController.create");
